51f6c25f63f817bfe7df842033c5cf75888b992f
Meldanor <Bhaals@gmx.de>
2013-04-21 21:05:07 +0200

    Initial commit

---
+++ src/main/java/de/minestar/conair/core/Core.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2013 MineStar.de 
+ * 
+ * This file is part of ConAir.
+ * 
+ * ConAir is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * ConAir is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ConAir.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.minestar.conair.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // TODO Auto-generated method stub
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO Auto-generated method stub
+        super.onDisable();
+    }
+}
