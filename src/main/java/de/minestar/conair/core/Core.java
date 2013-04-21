cd021aab56d209c8da87d2713de7819448992438
Meldanor <Bhaals@gmx.de>
2013-04-21 21:29:54 +0200

    Add first server/client structure

--- src/main/java/de/minestar/conair/core/Core.java
+++ src/main/java/de/minestar/conair/core/Core.java
@@ -18,22 +18,73 @@
 
 package de.minestar.conair.core;
 
+import java.util.logging.Logger;
+
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.minestar.conair.listener.ChatListener;
+import de.minestar.conair.network.ChatClient;
+import de.minestar.conair.network.ChatServer;
 
 public class Core extends JavaPlugin {
 
     public static final String NAME = "ConAir";
 
+    private boolean isServer;
+
+    private int port;
+    private String host;
+
+    private ChatServer chatServer;
+    private ChatClient chatClient;
+
     @Override
     public void onEnable() {
         enableListener(this.getServer().getPluginManager());
+        readConfig();
+
+        if (isServer) {
+            createChatServer();
+        }
+
+        createChatClient();
     }
 
     private void enableListener(PluginManager pm) {
         pm.registerEvents(new ChatListener(), this);
+    }
+
+    private void readConfig() {
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(this.getResource("config.yml"));
+
+        isServer = config.getBoolean("isServer", false);
+
+        port = config.getInt("port", 13371);
+        host = config.getString("host", "localhost");
+    }
+
+    private boolean createChatServer() {
+        try {
+            this.chatServer = new ChatServer();
+            this.getServer().getScheduler().runTaskAsynchronously(this, this.chatServer);
+            return true;
+        } catch (Exception e) {
+            Logger.getLogger(NAME).throwing("de.minestar.conair.core.Core", "createChatServer", e);
+            return false;
+        }
+    }
+
+    private boolean createChatClient() {
+        try {
+            this.chatClient = new ChatClient();
+            this.getServer().getScheduler().runTaskAsynchronously(this, this.chatClient);
+            return true;
+        } catch (Exception e) {
+            Logger.getLogger(NAME).throwing("de.minestar.conair.core.Core", "createChatClient", e);
+            return false;
+        }
     }
 
     @Override
